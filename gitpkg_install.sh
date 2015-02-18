8bed071680bf2eba09ec52d3bb6a640b0f50278c
Wen Li <wen.li@ucl.ac.uk>
2015-02-18 13:47:20 +0000

    Removing the source tarball when finished deploying the package

--- gitpkg_install.sh
+++ gitpkg_install.sh
@@ -7,3 +7,4 @@ dst=$2
 wget "https://github.com/${gitid}/archive/master.tar.gz" -O "${dst}/${pkg}.tar.gz"
 mkdir -p "${dst}/${pkg}"
 tar zxf "${dst}/${pkg}.tar.gz" --strip 1 -C "${dst}/${pkg}"
+rm "${dst}/${pkg}.tar.gz"
